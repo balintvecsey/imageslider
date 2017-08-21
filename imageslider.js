f80cd5f3ed0e26743567d1f8fd2011455a4bb3df
balintvecsey <balint.vecsey@gmail.com>
2017-08-21 11:15:07 +0200

    some polishing

--- imageslider.js
+++ imageslider.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const images = [
-  {title: 'Purrr, purrr', text: 'Refuse to drink water except out of someone\'s glass steal the warm chair right after you get up hopped up on catnip, and licks paws, and make meme, make cute face, meowwww.', url: 'https://placekitten.com/768/432'},
-  {title: 'Random', text: 'What, what', url: 'https://placeimg.com/640/360'},
-  {title: 'Murray', text: 'Uhuh ... yih!', url: 'https://fillmurray.com/640/360'},
-  {title: 'Crazy Cage', text: 'check out this dolizzle shizznit amizzle', url: 'https://placecage.com/c/640/360'},
-  {title: 'Normal Cage', text: 'crazy adipiscing dope', url: 'https://placecage.com/640/360'},
-  {title: 'Bear', text: 'Nullizzle sapizzle velit', url: 'https://placebear.com/640/360'},
-  {title: 'Bacon', text: 'check it out vizzle', url: 'https://baconmockup.com/640/360'},
-  {title: 'New1', text: 'check it out vizzle1', url: 'https://unsplash.it/640/360'},
-  {title: 'New2', text: 'check it out vizzle2', url: 'https://unsplash.it/g/640/360'},
-  {title: 'New3', text: 'check it out vizzle3', url: 'https://unsplash.it/640/360'},
-  {title: 'New4', text: 'check it out vizzle4', url: 'https://unsplash.it/g/640/360'},
-  {title: 'New5', text: 'check it out vizzle5', url: 'https://unsplash.it/640/360'},
+  {src: 'https://placekitten.com/768/432', title: 'Purrr, purrr', text: 'Refuse to drink water except out of someone\'s glass steal the warm chair right after you get up hopped up on catnip, and licks paws, and make meme, make cute face, meowwww.'},
+  {src: 'https://placeimg.com/640/360', title: 'Random', text: 'What, what'},
+  {src: 'https://fillmurray.com/640/360', title: 'Murray', text: 'Uhuh ... yih!'},
+  {src: 'https://placecage.com/c/640/360', title: 'Crazy Cage', text: 'check out this dolizzle shizznit amizzle'},
+  {src: 'https://placecage.com/640/360', title: 'Normal Cage', text: 'crazy adipiscing dope'},
+  {src: 'https://placebear.com/640/360', title: 'Bear', text: 'Nullizzle sapizzle velit'},
+  {src: 'https://baconmockup.com/640/360', title: 'Bacon', text: 'check it out vizzle'},
+  {src: 'https://unsplash.it/640/360', title: 'New1', text: 'check it out vizzle1'},
+  {src: 'https://unsplash.it/g/640/360', title: 'New2', text: 'check it out vizzle2'},
+  {src: 'https://unsplash.it/640/360', title: 'New3', text: 'check it out vizzle3'},
+  {src: 'https://unsplash.it/g/640/360', title: 'New4', text: 'check it out vizzle4'},
+  {src: 'https://unsplash.it/640/360', title: 'New5', text: 'check it out vizzle5'},
 ];
 
 let imageOrder = 0;
@@ -26,7 +26,7 @@ let rightButton = document.querySelector('.right');
 setLargePic();
 
 for (let i = 0; i < thumbnail.length; i++) {
-  thumbnail[i].src = images[i].url;
+  thumbnail[i].src = images[i].src;
   thumbnail[i].title = images[i].title;
   thumbnail[i].addEventListener('click', function() {
     changeSmallToLarge(i);
@@ -41,7 +41,7 @@ rightButton.addEventListener('click', function(){
 });
 
 function setLargePic() {
-  largeImage.src = images[imageOrder].url;
+  largeImage.src = images[imageOrder].src;
   largeImage.title = images[imageOrder].title;
   descriptionTitle.innerHTML = images[imageOrder].title;
   descriptionText.innerHTML = images[imageOrder].text;
@@ -65,7 +65,7 @@ function changeSmallToLarge(num) {
 };
 
 function setActive() {
-  for (var i = 0; i < thumbnail.length; i++) {
+  for (let i = 0; i < thumbnail.length; i++) {
     if (thumbnail[i].id === 'active') {
       thumbnail[i].removeAttribute('id');
     }
