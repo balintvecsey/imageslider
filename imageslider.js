d33ffb2a4c3ec810126cc1bbadb109922fc60db9
balintvecsey <balint.vecsey@gmail.com>
2017-08-23 14:45:37 +0200

    thumbnail can handle multiple images

--- imageslider.js
+++ imageslider.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const images = [
-  {src: 'https://placekitten.com/768/432', title: 'Purrr, purrr', text: 'Refuse to drink water except out of someone\'s glass steal the warm chair right after you get up hopped up on catnip, and licks paws, and make meme, make cute face, meowwww.'},
-  {src: 'https://placeimg.com/640/360', title: 'Random', text: 'What, what'},
-  {src: 'https://fillmurray.com/640/360', title: 'Murray', text: 'Uhuh ... yih!'},
-  {src: 'https://placecage.com/c/640/360', title: 'Crazy Cage', text: 'check out this dolizzle shizznit amizzle'},
-  {src: 'https://placecage.com/640/360', title: 'Normal Cage', text: 'crazy adipiscing dope'},
-  {src: 'https://placebear.com/640/360', title: 'Bear', text: 'Nullizzle sapizzle velit'},
-  {src: 'https://baconmockup.com/640/360', title: 'Bacon', text: 'check it out vizzle'},
-  {src: 'https://unsplash.it/640/360', title: 'New1', text: 'check it out vizzle1'},
-  {src: 'https://unsplash.it/g/640/360', title: 'New2', text: 'check it out vizzle2'},
-  {src: 'https://unsplash.it/640/360', title: 'New3', text: 'check it out vizzle3'},
-  {src: 'https://unsplash.it/g/640/360', title: 'New4', text: 'check it out vizzle4'},
-  {src: 'https://unsplash.it/640/360', title: 'New5', text: 'check it out vizzle5'},
+  {src: 'https://placekitten.com/768/432', title: 'Purrr, purrr', alt: 'Refuse to drink water except out of someone\'s glass steal the warm chair right after you get up hopped up on catnip, and licks paws, and make meme, make cute face, meowwww.'},
+  {src: 'https://placeimg.com/640/360', title: 'Random', alt: 'What, what'},
+  {src: 'https://fillmurray.com/640/360', title: 'Murray', alt: 'Uhuh ... yih!'},
+  {src: 'https://placecage.com/c/640/360', title: 'Crazy Cage', alt: 'check out this dolizzle shizznit amizzle'},
+  {src: 'https://placecage.com/640/360', title: 'Normal Cage', alt: 'crazy adipiscing dope'},
+  {src: 'https://placebear.com/640/360', title: 'Bear', alt: 'Nullizzle sapizzle velit'},
+  {src: 'https://baconmockup.com/640/360', title: 'Bacon', alt: 'check it out vizzle'},
+  {src: 'https://unsplash.it/640/360', title: 'New1', alt: 'check it out vizzle1'},
+  {src: 'https://unsplash.it/g/640/360', title: 'New2', alt: 'check it out vizzle2'},
+  {src: 'https://unsplash.it/640/360', title: 'New3', alt: 'check it out vizzle3'},
+  {src: 'https://unsplash.it/g/640/360', title: 'New4', alt: 'check it out vizzle4'},
+  {src: 'https://unsplash.it/640/360', title: 'New5', alt: 'check it out vizzle5'},
 ];
 
+let difference = 0;
 let imageOrder = 0;
+let thumbnailOrder = 0;
 let thumbnail = document.querySelectorAll('.small');
 let largeImage = document.querySelector('.large');
 let descriptionTitle = document.querySelector('h2');
@@ -23,15 +25,8 @@ let descriptionText = document.querySelector('p');
 let leftButton = document.querySelector('.left');
 let rightButton = document.querySelector('.right');
 
+setThumbnail();
 setLargePic();
-
-for (let i = 0; i < thumbnail.length; i++) {
-  thumbnail[i].src = images[i].src;
-  thumbnail[i].title = images[i].title;
-  thumbnail[i].addEventListener('click', function() {
-    changeSmallToLarge(i);
-  })
-}
 
 leftButton.addEventListener('click', function(){
   changePic(-1);
@@ -40,27 +35,53 @@ rightButton.addEventListener('click', function(){
   changePic(1);
 });
 
+for (let i = 0; i < thumbnail.length; i++) {
+  thumbnail[i].addEventListener('click', function() {
+    changeSmallToLarge(i);
+  })
+};
+
+function setThumbnail() {
+  imageOrder = thumbnailOrder + difference;
+  if(imageOrder === images.length) {
+    difference = 0;
+    thumbnailOrder = 0;
+  } else if(imageOrder < 0) {
+    difference = images.length - thumbnail.length;
+    thumbnailOrder = thumbnail.length - 1;
+  }
+
+  for (let i = 0; i < thumbnail.length; i++) {
+    thumbnail[i].src = images[i + difference].src;
+    thumbnail[i].title = images[i + difference].title;
+    thumbnail[i].alt = images[i + difference].alt;
+  }
+};
+
 function setLargePic() {
-  largeImage.src = images[imageOrder].src;
-  largeImage.title = images[imageOrder].title;
-  descriptionTitle.innerHTML = images[imageOrder].title;
-  descriptionText.innerHTML = images[imageOrder].text;
+  largeImage.src = thumbnail[thumbnailOrder].src;
+  largeImage.title = thumbnail[thumbnailOrder].title;
+  descriptionTitle.innerHTML = thumbnail[thumbnailOrder].title;
+  descriptionText.innerHTML = thumbnail[thumbnailOrder].alt;
   setActive();
-  console.log(imageOrder);
+  console.log(thumbnailOrder);
 };
 
 function changePic(direction) {
-  imageOrder += direction;
-  if(imageOrder === images.length) {
-    imageOrder = 0;
-  } else if(imageOrder < 0) {
-    imageOrder = images.length - 1;
+  thumbnailOrder += direction;
+  if(thumbnailOrder > thumbnail.length - 1) {
+    difference++;
+    thumbnailOrder = thumbnail.length - 1;
+  } else if (thumbnailOrder < 0) {
+    difference--;
+    thumbnailOrder = 0;
   }
+  setThumbnail();
   setLargePic();
 };
 
 function changeSmallToLarge(num) {
-  imageOrder = num;
+  thumbnailOrder = num;
   setLargePic();
 };
 
@@ -70,5 +91,5 @@ function setActive() {
       thumbnail[i].removeAttribute('id');
     }
   }
-  thumbnail[imageOrder].id = 'active';
+  thumbnail[thumbnailOrder].id = 'active';
 };
